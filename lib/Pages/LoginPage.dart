bfa841889d92e2faed6ed589c108b0c66862410d
André Alves <andredasillvaa376@gmail.com>
2024-05-14 07:55:12 -0300

    update parcial

--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -5,8 +5,10 @@ import 'package:barberbooking/Components/Button.dart';
 import 'package:barberbooking/Components/InputText.dart';
 import 'package:barberbooking/Components/Label.dart';
 import 'package:barberbooking/Pages/RegisterPage.dart';
+import 'package:barberbooking/Pages/RememberPage.dart';
 
-void Function() navegarLoginPage(BuildContext context) { // Altere o tipo de retorno para void Function()
+void Function() navegarLoginPage(BuildContext context) {
+  // Altere o tipo de retorno para void Function()
   return () {
     Navigator.push(
       context,
@@ -14,7 +16,9 @@ void Function() navegarLoginPage(BuildContext context) { // Altere o tipo de ret
     );
   };
 }
-void Function() navegarRegisterPage(BuildContext context) { // Altere o tipo de retorno para void Function()
+
+void Function() navegarRegisterPage(BuildContext context) {
+  // Altere o tipo de retorno para void Function()
   return () {
     Navigator.push(
       context,
@@ -22,6 +26,17 @@ void Function() navegarRegisterPage(BuildContext context) { // Altere o tipo de 
     );
   };
 }
+
+void Function() navegarRememberPage(BuildContext context) {
+  // Altere o tipo de retorno para void Function()
+  return () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => RememberPage()),
+    );
+  };
+}
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
@@ -29,26 +44,87 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center( // Centraliza a Column verticalmente
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          // Centraliza a Column verticalmente
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Label(text: "Bem-vindo!!!", fontSize: 30, color: Color(0xFF6B7280),),
-              const Label(text: "Faça login para continuar", fontSize: 15, color: Color(0xFF6B7280),),
-              const Label(text: "Por favor informe seu e-mail e senha para entrar na sua conta.", fontSize: 15, color: Color(0xFF6B7280), width: 325,),
-              const InputText(text: "Usuário", width: 330),
-              const InputText(text: "Senha", width: 330),
-              const InputText(text: "Esqueceu a senha?", width: 330),
-              const Button(text: "Entrar",route: HomePage(),),
-
-              Row(
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'assets/pictures/logo.jpeg',
+                    width: 200,
+                    height: 200,
+                  ),
+                  const Label(
+                    text: "Bem-vindo!!!",
+                    fontSize: 28,
+                    color: Color(0xFF6B7280),
+                  ),
+                  const Label(
+                    text: "Faça login para continuar",
+                    fontSize: 14,
+                    color: Color(0xFF6B7280),
+                  ),
+                  const Label(
+                    text:
+                        "Por favor informe seu e-mail e senha para entrar na sua conta.",
+                    fontSize: 16,
+                    color: Color(0xFF6B7280),
+                    width: 325,
+                  ),
+                  const Label(
+                    text: "Usuário*",
+                    fontSize: 14,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                  const InputText(text: "Usuário", width: 330),
+                  const Label(
+                    text: "Senha",
+                    fontSize: 14,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                  const InputText(text: "Senha", width: 330),
+                  TextButton(
+                    onPressed: navegarRememberPage(
+                        context), // Altere o tipo de retorno para void Function()
+                    child: const Label(
+                      text: "Esqueceu a senha?",
+                      fontSize: 14,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Label(text: "Não tem uma conta?", fontSize: 15, color: Color(0xFF6B7280),),
-                  TextButton(
-                    onPressed: navegarRegisterPage(context), // Altere o tipo de retorno para void Function()
-                    child: const Label(text: "Registre-se", fontSize: 15, color: Color(0xFF6B7280),),
+                  const Button(
+                    text: "Entrar",
+                    route: HomePage(),
                   ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Label(
+                        text: "Não tem uma conta?",
+                        fontSize: 14,
+                        color: Color(0xFF6B7280),
+                      ),
+                      TextButton(
+                        onPressed: navegarRegisterPage(
+                            context), // Altere o tipo de retorno para void Function()
+                        child: const Label(
+                          text: "Registre-se",
+                          fontSize: 14,
+                          color: Color(0xFF6B7280),
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               )
             ],
